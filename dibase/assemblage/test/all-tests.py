0f2dfd2beb73d36c1e7d8f4c93bde3088baac4a3
Ralph McArdell <ralph@dibase.co.uk>
2015-06-23 13:47:08 +0100

    WIP : Added runner for all tests
    Work In Progress : added runner program to execute tests from all (near)
    unit tests.

---
+++ dibase/assemblage/test/all-tests.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+# v3.4+
+"""
+Run all per-module quick but not-all-are-quite-unit-tests.
+"""
+import unittest
+import inspect
+
+if __name__ == '__main__':
+  test_info = [ ('assemblage-Assemblage-tests', 'TestAssemblageAssemblage')
+              , ('assemblage-Blueprint-tests', 'TestAssemblageBlueprint')
+              , ('assemblage-Component-tests', 'TestAssemblageComponent')
+              , ('assemblage-DigestCache-tests', 'TestAssemblageDigestCache')
+              , ('assemblage-ShelfDigestStore-tests', 'TestAssemblageDigestStore')
+              , ('assemblage-FileComponent-tests', 'TestAssemblageFileComponent')
+              ]
+  ldr = unittest.TestLoader()
+  test_suites = []
+  for t in test_info:
+    classes = inspect.getmembers( __import__(t[0])
+                                , lambda obj : inspect.isclass(obj) and obj.__name__ == t[1]
+                                )
+    test_suites.append(ldr.loadTestsFromTestCase(classes[0][1]))
+  all_tests = unittest.TestSuite(test_suites)
+  unittest.TextTestRunner().run(all_tests.run)
