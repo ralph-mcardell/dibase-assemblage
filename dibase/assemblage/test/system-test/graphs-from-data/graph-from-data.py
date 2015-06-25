5d2ff9f5b7c09f67b089bd82008f625b92d010a5
Ralph McArdell <ralph@dibase.co.uk>
2015-06-25 16:18:18 +0100

    WIP : Whitespace change correcting indentation
    Work In Progress : Removed 1 tab of indent from CSVDataMunger and
    DirectoryComponent as they were overly indented.

--- dibase/assemblage/test/system-test/graphs-from-data/graph-from-data.py
+++ dibase/assemblage/test/system-test/graphs-from-data/graph-from-data.py
@@ -57,114 +57,113 @@ class build: # action class
     return element.doesNotExist() or element.isOutOfDate()
 
 class CSVDataMunger(Component):
-    def __init__(self, name, assemblage, elements=[], logger=None, transformer=None):
-      '''
-      Passes all parameters but transformer on to the Component base.
-      The transformer parameter should specify a data transformation callable.
-      Expects to munge exactly 1 (sub-)element representing the CSV file to munge
-      data from.
-      '''
-      self.inputFilePath = None
-      for e in elements:
-        if type(e) is FileComponent:
-          if self.inputFilePath:
-            raise RuntimeError("CSVDataMunger: more than 1 input FileComponent elements provided")
-          self.inputFilePath = e.normalisedPath()
-      if not self.inputFilePath:
-        raise RuntimeError("CSVDataMunger: No input FileComponent element provided" )
+  def __init__(self, name, assemblage, elements=[], logger=None, transformer=None):
+    '''
+    Passes all parameters but transformer on to the Component base.
+    The transformer parameter should specify a data transformation callable.
+    Expects to munge exactly 1 (sub-)element representing the CSV file to munge
+    data from.
+    '''
+    self.inputFilePath = None
+    for e in elements:
+      if type(e) is FileComponent:
+        if self.inputFilePath:
+          raise RuntimeError("CSVDataMunger: more than 1 input FileComponent elements provided")
+        self.inputFilePath = e.normalisedPath()
+    if not self.inputFilePath:
+      raise RuntimeError("CSVDataMunger: No input FileComponent element provided" )
 
-      super().__init__(name,assemblage,elements,logger)
-      self.path = None # filled in as late as possible
-      self.xform = transformer
+    super().__init__(name,assemblage,elements,logger)
+    self.xform = transformer
 
-    def doesNotExist(self):
-      return not os.path.exists(str(self))
+  def doesNotExist(self):
+    return not os.path.exists(str(self))
 
-    def build_afterElementsActions(self):
-      '''
-      build action action-steps to be performed after this element's elements
-      have applied the build action.
-      
-      The actions are:
-        - read the CSV file into memory as a CSV map
-        - pass the CSV map to the transformer function which should
-          return another map in the form:
-            { '' : (dataset-name', 'dataset file name')
-            , 'grpname-0' : { '' : ('group 0 name', 'group 0 file name')
-                            ,'field-0' : value-0
-                            , field-1' : value-1
-                            , ...
-                            , 'field-n' : value-n
-                            }
-            , 'grpname-1' : { '' : ('group 1 name', 'group 1 file name')
-                            ,'field-0' : value-0
-                            , field-1' : value-1
-                            , ...
-                            , 'field-n' : value-n
-                            }
-            , ...
-            , 'grpname-n' : { '' : ('group n name', 'group n file name')
-                            ,'field-0' : value-0
-                            , field-1' : value-1
-                            , ...
-                            , 'field-n' : value-n
-                            }
-            }
-        - walk the map of maps and write a CVS file for each group map and
-          a master dataset CSV file with records giving the group names and
-          file pathnames - with the first record giving the dataset name.
-      '''
-      data_to_write = {}
-      path = self.elementAttribute(0,'normalisedPath')()
-      self.debug("Reading raw data from CSV file: '%s'" % path )
-      with open(path) as csvfile:
-        dialect = csv.Sniffer().sniff(csvfile.read(4096))
-        csvfile.seek(0)
-        reader = csv.reader(csvfile, dialect)
-        data_to_write = self.xform(reader)
+  def build_afterElementsActions(self):
+    '''
+    build action action-steps to be performed after this element's elements
+    have applied the build action.
+    
+    The actions are:
+      - read the CSV file into memory as a CSV map
+      - pass the CSV map to the transformer function which should
+        return another map in the form:
+          { '' : (dataset-name', 'dataset file name')
+          , 'grpname-0' : { '' : ('group 0 name', 'group 0 file name')
+                          ,'field-0' : value-0
+                          , field-1' : value-1
+                          , ...
+                          , 'field-n' : value-n
+                          }
+          , 'grpname-1' : { '' : ('group 1 name', 'group 1 file name')
+                          ,'field-0' : value-0
+                          , field-1' : value-1
+                          , ...
+                          , 'field-n' : value-n
+                          }
+          , ...
+          , 'grpname-n' : { '' : ('group n name', 'group n file name')
+                          ,'field-0' : value-0
+                          , field-1' : value-1
+                          , ...
+                          , 'field-n' : value-n
+                          }
+          }
+      - walk the map of maps and write a CVS file for each group map and
+        a master dataset CSV file with records giving the group names and
+        file pathnames - with the first record giving the dataset name.
+    '''
+    data_to_write = {}
+    path = self.elementAttribute(0,'normalisedPath')()
+    self.debug("Reading raw data from CSV file: '%s'" % path )
+    with open(path) as csvfile:
+      dialect = csv.Sniffer().sniff(csvfile.read(4096))
+      csvfile.seek(0)
+      reader = csv.reader(csvfile, dialect)
+      data_to_write = self.xform(reader)
 #      self.debug("Data to be written to CSV files:\n%s" % str(data_to_write))
-      main_filename = str(self)
-      outfilename_stem = os.path.splitext(main_filename)[0]
-      self.debug("Writing output data to main file %(m)s and member files with names starting '%(s)s'" 
-                  % {'m':main_filename, 's': outfilename_stem}
-                )
-      with open(main_filename,'w', newline='') as mainfile:
-        main_writer = csv.writer(mainfile)
-        main_writer.writerow(['Group','Member','File'])
-        for grp_name,grp_data in data_to_write.items():
-          if grp_name=='__METADATA__':
-            main_writer.writerow([grp_name,outfilename_stem, grp_data['__TITLE__']])
-          else:
-              for mbr_name,mbr_data in grp_data.items():
-                mbr_filename = '.'.join([outfilename_stem,mbr_data['__FILE__'],'csv'])
-                with open(mbr_filename,'w', newline='') as mbrfile:
-                  mbr_writer = csv.writer(mbrfile)
-                  mbr_writer.writerow(['Price'])
-                  for price in mbr_data['__DATA__']:
-                    mbr_writer.writerow([price])
-                main_writer.writerow([grp_name,mbr_name,mbr_filename])
-      self.assemblage().digestCache().writeBack()
+    main_filename = str(self)
+    outfilename_stem = os.path.splitext(main_filename)[0]
+    self.debug("Writing output data to main file %(m)s and member files with names starting '%(s)s'" 
+                % {'m':main_filename, 's': outfilename_stem}
+              )
+    with open(main_filename,'w', newline='') as mainfile:
+      main_writer = csv.writer(mainfile)
+      main_writer.writerow(['Group','Member','File'])
+      for grp_name,grp_data in data_to_write.items():
+        if grp_name=='__METADATA__':
+          main_writer.writerow([grp_name,outfilename_stem, grp_data['__TITLE__']])
+        else:
+            for mbr_name,mbr_data in grp_data.items():
+              mbr_filename = '.'.join([outfilename_stem,mbr_data['__FILE__'],'csv'])
+              with open(mbr_filename,'w', newline='') as mbrfile:
+                mbr_writer = csv.writer(mbrfile)
+                mbr_writer.writerow(['Price'])
+                for price in mbr_data['__DATA__']:
+                  mbr_writer.writerow([price])
+              main_writer.writerow([grp_name,mbr_name,mbr_filename])
+    self.assemblage().digestCache().writeBack()
 
 class DirectoryComponent(Component):
-    '''
-    A simple sub-type of Component that ensures directories are created if
-    they do not exist regardless of action so long as an action query function
-    calls doesNotExist.
-    '''
-    def __init__(self, name, assemblage, elements=[], logger=None):
-      super().__init__(name,assemblage,elements,logger)
+  '''
+  A simple sub-type of Component that ensures directories are created if
+  they do not exist regardless of action so long as an action query function
+  calls doesNotExist.
+  '''
+  def __init__(self, name, assemblage, elements=[], logger=None):
+    super().__init__(name,assemblage,elements,logger)
 
-    def doesNotExist(self):
-      '''
-      Unlike most doesNotExist methods the DirectoryComponent implementation
-      uses a call to doesNotExist to ensure the directory does exist thus
-      allowing it to be used with any action that queries doesNotExist.
-      '''
-      if not os.path.exists(str(self)):
-        os.mkdir(str(self))
-      if not os.path.exists(str(self)):
-        raise OSError("Failed to create directory '%s'"%str(self))
-      return False
+  def doesNotExist(self):
+    '''
+    Unlike most doesNotExist methods the DirectoryComponent implementation
+    uses a call to doesNotExist to ensure the directory does exist thus
+    allowing it to be used with any action that queries doesNotExist.
+    '''
+    if not os.path.exists(str(self)):
+      os.mkdir(str(self))
+    if not os.path.exists(str(self)):
+      raise OSError("Failed to create directory '%s'"%str(self))
+    return False
 
 def MungeSalesJan2009(records):
   first_row = True
