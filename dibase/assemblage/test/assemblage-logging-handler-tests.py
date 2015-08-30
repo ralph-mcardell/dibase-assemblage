49e5e2a566e97551abe1435c7b995fa3e7dae6bc
Ralph McArdell <ralph@dibase.co.uk>
2015-08-30 10:38:13 +0100

    WIP : Tidied handlers init test; alter logged levels test (Partial OK)
    Work In Progress : Tidied initialisation of known handlers test to
    capture messages to string IO and check messages formatted OK.
    Added test to see if allowed logged levels can be altered after known
    handler initialisation - confirmed that work to be done to facility
    additional allowed logged levels.

--- dibase/assemblage/test/assemblage-logging-handler-tests.py
+++ dibase/assemblage/test/assemblage-logging-handler-tests.py
@@ -16,21 +16,19 @@ project_root_dir = os.path.dirname(
                   )          # project directory
 if project_root_dir not in sys.path:
   sys.path.insert(0, project_root_dir)
-from dibase.assemblage.logging import handler 
-from dibase.assemblage.logging import OUTPUT 
-from logging import INFO 
-from logging import WARNING 
-from logging import ERROR 
+from dibase.assemblage.logging import handler
+from dibase.assemblage.logging import OUTPUT
+from dibase.assemblage.logging import Allow
+from dibase.assemblage.logging import Deny
+from dibase.assemblage.logging import specifyLoggedLevels
+from dibase.assemblage.logging import logLevelsFor
+from logging import INFO
+from logging import WARNING
+from logging import ERROR
 from logging import CRITICAL 
-from logging import LogRecord  
-
-#from dibase.assemblage.logging import Allow
-#from dibase.assemblage.logging import Deny
-#from dibase.assemblage.logging import configureLogger
-#from dibase.assemblage.logging import addHandler
-#from dibase.assemblage.logging import specifyLoggedLevels
-#from dibase.assemblage.logging import logLevelsFor
-#from dibase.assemblage.logging import setFormatter
+from logging import LogRecord
+from logging import getLevelName
+from io import StringIO
 
 class Record:
   def __init__(self, level, args=[]):
@@ -41,16 +39,26 @@ def makeLogRecord(msg, level):
 
 class TestAssemblageLoggingKnownHandlers(unittest.TestCase):
   def test_000_known_handlers_are_initialised_as_expected_by_handler_initHandler(self):
+  # *MUST* be first test as it performs required one-time initialisation under test
     message = "this is the message"
     self.assertIsNone(handler.stdout)
     self.assertIsNone(handler.stderr)
+    sstrmOut = StringIO()
+    sstrmErr = StringIO()
+    old_stdout = sys.stdout
+    sys.stdout = sstrmOut
+    old_stderr = sys.stdout
+    sys.stderr = sstrmErr 
     handler._initHandlers()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
     self.assertIsNotNone(handler.stdout)
     self.assertIsNotNone(handler.stderr)
     self.assertFalse(handler.stdout.filter(Record(OUTPUT-1)))
     self.assertTrue(handler.stdout.filter(Record(OUTPUT)))
     self.assertFalse(handler.stdout.filter(Record(OUTPUT+1)))
     self.assertTrue(handler.stdout.handle(makeLogRecord(message,OUTPUT)))
+    self.assertEqual(sstrmOut.getvalue(), "%s\n"%message)
     self.assertFalse(handler.stderr.filter(Record(INFO-1)))
     self.assertTrue(handler.stderr.filter(Record(INFO)))
     self.assertTrue(handler.stderr.filter(Record(WARNING)))
@@ -58,6 +66,22 @@ class TestAssemblageLoggingKnownHandlers(unittest.TestCase):
     self.assertTrue(handler.stderr.filter(Record(CRITICAL)))
     self.assertFalse(handler.stderr.filter(Record(CRITICAL+1)))
     self.assertTrue(handler.stderr.handle(makeLogRecord(message,WARNING)))
+    self.assertRegex(sstrmErr.getvalue(), "^%s.*%s$"%(getLevelName(WARNING),message))
+  def test_known_handler_can_have_allowed_logged_levels_altered_post_initialisation(self):
+    message = "this is the message"
+    self.assertIsNotNone(handler.stdout)
+    specifyLoggedLevels(logLevelsFor(None, Deny(lambda lvl:lvl==OUTPUT)))(handler.stdout)
+    self.assertFalse(handler.stdout.filter(Record(OUTPUT-1)))
+    self.assertFalse(handler.stdout.filter(Record(OUTPUT)))
+    self.assertFalse(handler.stdout.filter(Record(OUTPUT+1)))
+### TODO : Fix inability to allow additional levels after known handler initialisation
+#    specifyLoggedLevels(logLevelsFor(None, Allow(lambda lvl:lvl==WARNING)))(handler.stdout)
+#    self.assertFalse(handler.stdout.filter(Record(OUTPUT-1)))
+#    self.assertFalse(handler.stdout.filter(Record(OUTPUT)))
+#    self.assertFalse(handler.stdout.filter(Record(OUTPUT+1)))
+#    self.assertFalse(handler.stdout.filter(Record(WARNING-1)))
+#    self.assertTrue(handler.stdout.filter(Record(WARNING)))
+#    self.assertFalse(handler.stdout.filter(Record(WARNING+1)))
 
 if __name__ == '__main__':
   unittest.main()
